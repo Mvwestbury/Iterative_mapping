2e4375dd1a09294d4103e3dbf0aac9614ef4b3f0
Mick Westbury <67960396+Mvwestbury@users.noreply.github.com>
2021-11-23 16:39:07 +0100

    Add files via upload

---
+++ MITObim/MITObimming.sh
@@ -0,0 +1,13 @@
+## $1 - fastq files
+## $2 - ID
+## $3 - ref file
+## $4 - ref ID
+## $5 - results folder
+
+for num in 0 1 3 5 10 15
+do
+rm -r $5/$4_mismatch${num}
+mkdir $5/$4_mismatch${num}
+cd $5/$4_mismatch${num}
+MITObim -end 1000 -sample $2 -readpool $1 -ref $4_${num}perc --quick $3 --clean --missmatch ${num} > $2-$4_${num}perc.log
+done
