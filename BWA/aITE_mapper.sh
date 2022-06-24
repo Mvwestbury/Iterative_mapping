6593c77bc71ce11f39c51b274cb69056c0f61b7d
Mick Westbury <67960396+Mvwestbury@users.noreply.github.com>
2022-06-24 11:38:44 +0200

    Update aITE_mapper.sh

--- BWA/aITE_mapper.sh
+++ BWA/aITE_mapper.sh
@@ -26,8 +26,7 @@ samtools depth -a $4/$3.1.rmdup.sort.bam | awk '$3==0{print}' | wc -l >> $4/$3_$
 for num in {1..100}
 do
 samtools index $4/$3.${num}.rmdup.sort.bam 
-angsd -dofasta 3 -minq 30 -minmapq $7 -i $4/$3.${num}.rmdup.sort.bam -out $4/$3_$6.con.${num} -docounts 1 -setMinDepth 3
-gunzip $4/$3_$6.con.${num}.fa.gz
+kindel consensus --min-depth 3 $4/$3.${num}.rmdup.sort.bam > $4/$3_$6.con.${num}.fa 
 bwa index $4/$3_$6.con.${num}.fa
 NUM=$(echo ${num} | awk '{print $1+1}')
 bwa aln -l 999 -n 0.04 -t $1 $4/$3_$6.con.${num}.fa $2 | bwa samse $4/$3_$6.con.${num}.fa - $2 | samtools view -F 4 -q $7 -@ $1 -uS - | samtools sort -@ $1 -o $4/$3.sort.bam
@@ -40,14 +39,14 @@ samtools view -c $4/$3.${NUM}.rmdup.sort.bam >> $4/$3_$6.records.txt
 samtools depth -a $4/$3.${NUM}.rmdup.sort.bam | awk '{sum+=$3;cnt++}END{print sum/cnt "\t" sum}' >> $4/$3_$6.records.txt 
 samtools depth -a $4/$3.${NUM}.rmdup.sort.bam | awk '$3==0{print}' | wc -l >> $4/$3_$6.records.txt
 
-samtools view -c $4/$3.${num}.rmdup.sort.bam > $4/mapped_reads.tmp
-samtools view -c $4/$3.${NUM}.rmdup.sort.bam > $4/mapped_reads2.tmp
+samtools view -c $4/$3.${num}.rmdup.sort.bam > $4/$3.mapped_reads.tmp
+samtools view -c $4/$3.${NUM}.rmdup.sort.bam > $4/$3.mapped_reads2.tmp
 $4/$3.${num}.rmdup.sort.bam*
-N1=$(cat $4/mapped_reads.tmp)
-N2=$(cat $4/mapped_reads2.tmp)
+N1=$(cat $4/$3.mapped_reads.tmp)
+N2=$(cat $4/$3.mapped_reads2.tmp)
 if [ $N1 = $N2 ]; then
 echo Finished after ${num} iterations with $N1 reads mapping        
-angsd -dofasta 3 -minq 30 -minmapq 30 -i $4/$3.${NUM}.rmdup.sort.bam -out $4/$3_$6.con.${NUM}_final -docounts 1 -setMinDepth 3
+kindel consensus --min-depth 3 $4/$3.${NUM}.rmdup.sort.bam > $4/$3_$6.con.${NUM}_final.fasta
 break
 fi
 
