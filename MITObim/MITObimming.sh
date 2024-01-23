45337a596ada0d0508c7839b7136f31a4f545280
Mick Westbury <67960396+Mvwestbury@users.noreply.github.com>
2024-01-23 13:46:06 +0100

    Update MITObimming.sh

--- MITObim/MITObimming.sh
+++ MITObim/MITObimming.sh
@@ -9,5 +9,5 @@ do
 rm -r $5/$4_mismatch${num}
 mkdir $5/$4_mismatch${num}
 cd $5/$4_mismatch${num}
-MITObim -end 1000 -sample $2 -readpool $1 -ref $4_${num}perc --quick $3 --clean --missmatch ${num} > $2-$4_${num}perc.log
+MITObim -end 1000 -sample $2 -readpool $1 -ref $4_${num}perc --quick $3 --clean --mismatch ${num} > $2-$4_${num}perc.log
 done
